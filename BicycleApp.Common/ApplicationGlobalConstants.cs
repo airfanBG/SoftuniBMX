545b003ff2987a33e5b71faab24ffa04dd2be287
Krasimir Iliev <kvi_2012@abv.bg>
2023-11-05 00:40:44 +0200

    Add service for saving and deleting pictures

---
+++ BicycleApp.Common/ApplicationGlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace BicycleApp.Common
+{
+    public static class ApplicationGlobalConstants
+    {
+        public const int DefaultMaxCountOfPicturesInOneFolder = 10;
+
+        public const string ClientsMainFolderName = "clients";
+
+        public const string EmployeesMainFolderName = "employees";
+
+        public const string PartsMainFolderName = "parts";
+    }
+}
---
+++ BicycleApp.Services/Services/Interfaces/IPictureOrganizerServices.cs
@@ -0,0 +1,13 @@
+﻿namespace BicycleApp.Services.Services.Interfaces
+{
+    public interface IPictureOrganizerServices
+    {
+        string SaveClientPicture(byte[] file, string fileType, string roothFilePath, string clientIdentification);
+
+        string SaveEmployeePicture(byte[] file, string fileType, string roothFilePath, string employeeIdentitycation);
+
+        string SavePartPicture(byte[] file, string fileType, string roothFilePath, string partIdentification);
+
+        string DeleteFile(string filePath);
+    }
+}
