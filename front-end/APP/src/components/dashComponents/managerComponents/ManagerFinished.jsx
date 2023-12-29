691b4d3838184ef4a820f8999595038bc7199e76
yuchormanski <yuchormanski@gmail.com>
2023-12-29 22:55:10 +0200

    styling

--- front-end/APP/src/components/dashComponents/managerComponents/ManagerFinished.jsx
+++ front-end/APP/src/components/dashComponents/managerComponents/ManagerFinished.jsx
@@ -13,83 +13,79 @@ import FinishedOrder from "./FinishedOrder.jsx";
 import Popup from "../../Popup.jsx";
 
 function ManagerFinished() {
-
-   
   const [background, setBackground] = useState(false);
   const [currentOrder, setCurrentOrder] = useState({});
-  
+
   const [error, setError] = useState({});
   const [loading, setLoading] = useState(false);
   const [orderList, setOrderList] = useState([]);
 
-    // State to hold user input
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
+  // State to hold user input
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
-    const abortController = new AbortController();
+  const abortController = new AbortController();
 
-    async function getFinishedOrders(e) {
-      e.preventDefault();
-      setLoading(true);
-      const queryString = `?startDate=${startDate}&endDate=${endDate}`;
-      const result = await get(environment.finished_orders + queryString);
-      if (!result) {
-        setLoading(false);
-        return setError({
-          message: "Something went wrong. Service can not get data!",
-        });
-      }
-      setOrderList(result);
+  async function getFinishedOrders(e) {
+    e.preventDefault();
+    setLoading(true);
+    const queryString = `?startDate=${startDate}&endDate=${endDate}`;
+    const result = await get(environment.finished_orders + queryString);
+    if (!result) {
       setLoading(false);
-  
-      if (orderList.length === 0)
-      return <h2>There is no orders in this category</h2>;
+      return setError({
+        message: "Something went wrong. Service can not get data!",
+      });
     }
-    
-    function onOrderButtonClick(o) {
-      setCurrentOrder(o);
-      setBackground(true);
-    }
+    setOrderList(result);
+    setLoading(false);
 
-    function close(e) {
-      setCurrentOrder({});
-      setBackground(false);
-    }
-    
+    if (orderList.length === 0)
+      return <h2>There is no orders in this category</h2>;
+  }
+
+  function onOrderButtonClick(o) {
+    setCurrentOrder(o);
+    setBackground(true);
+  }
+
+  function close(e) {
+    setCurrentOrder({});
+    setBackground(false);
+  }
+
   return (
     <>
       <section className={styles.board}>
         <BoardHeader />
         {loading && <LoaderWheel />}
 
-        <h2>Select time period:</h2>
+        <h2 className={styles.boardHeading}>Select time period:</h2>
 
         <section className={styles.section}>
-        <form className={styles.form}>
-      <label className={styles.label}>
-        Start Date:
-        <input
-          className={styles.input}
-          type="date"
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
-        />
-      </label>
-      <br />
-      <label className={styles.label}>
-        End Date:
-        <input
-          className={styles.input}
-          type="date"
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)}
-        />
-      </label>
-      <br />
-      <button className={styles.btnAdd} onClick={getFinishedOrders}>
-      Get Orders
-      </button>
-    </form>
+          <form className={styles.form}>
+            <label className={styles.label}>
+              Start Date:
+              <input
+                className={styles.input}
+                type="date"
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+            </label>
+            <label className={styles.label}>
+              End Date:
+              <input
+                className={styles.input}
+                type="date"
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+            </label>
+            <button className={styles.btnAdd} onClick={getFinishedOrders}>
+              Get Orders
+            </button>
+          </form>
         </section>
       </section>
 
@@ -102,7 +98,6 @@ function ManagerFinished() {
         Orders in sequence by time of creation
       </h2>
       <section className={styles.board}>
-        <BoardHeader />
         <div className={styles.orders}>
           {loading && <LoaderWheel />}
           {orderList.map((order, i) => (
