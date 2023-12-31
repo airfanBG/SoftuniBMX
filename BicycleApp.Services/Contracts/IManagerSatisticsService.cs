3107c33ab0843866206d98f955109c254dea8bdf
Todorov135 <t.todorov135@gmail.com>
2023-12-31 10:46:24 +0200

    Add past and current month work time by employee.

---
+++ BicycleApp.Services/Contracts/IManagerSatisticsService.cs
@@ -0,0 +1,9 @@
+﻿namespace BicycleApp.Services.Contracts
+{
+    using BicycleApp.Services.Models.ManagerStatistics;
+
+    public interface IManagerSatisticsService
+    {
+        Task<PastAndCurrentEmployeeWorkingMinutesDto> GetEmployeeOutputForThePastAndCurrentMonth(string employeeId);
+    }
+}
---
+++ BicycleApp.Services/Services/ManagerSatisticsService.cs
@@ -0,0 +1,50 @@
+﻿namespace BicycleApp.Services.Services
+{
+    using BicicleApp.Common.Providers.Contracts;
+    using BicycleApp.Data;
+    using BicycleApp.Services.Contracts;
+    using BicycleApp.Services.Models.ManagerStatistics;
+    using Microsoft.EntityFrameworkCore;
+    using System.Threading.Tasks;
+
+    public class ManagerSatisticsService : IManagerSatisticsService
+    {
+        private readonly BicycleAppDbContext _db;
+        private readonly IDateTimeProvider _dateTimeProvider;
+        public ManagerSatisticsService(BicycleAppDbContext db, IDateTimeProvider dateTimeProvider)
+        {
+            _db = db;
+            _dateTimeProvider = dateTimeProvider;
+        }
+        public async Task<PastAndCurrentEmployeeWorkingMinutesDto> GetEmployeeOutputForThePastAndCurrentMonth(string employeeId)
+        {
+            var pastMonthValue = _dateTimeProvider.PreviousMonthObject;
+            var currentMonthValue = _dateTimeProvider.Now;
+
+            var pastMonthEmployeeWorkingMinutes = await _db.OrdersPartsEmployees
+                                                           .AsNoTracking()
+                                                           .Where(e => e.EmployeeId == employeeId
+                                                                       && e.StartDatetime.Value.Month == pastMonthValue.PreviousMonth
+                                                                       && e.StartDatetime.Value.Year == pastMonthValue.PreviousYear)
+                                                           .SelectMany(a => a.OrdersPartsEmployeesInfos.Select(x => x.ProductionТime))
+                                                           .SumAsync(a => a.Minutes);
+
+            var currentMonthEmployeeWorkingMinutes = await _db.OrdersPartsEmployees
+                                                           .AsNoTracking()
+                                                           .Where(e => e.EmployeeId == employeeId
+                                                                       && e.StartDatetime.Value.Month == currentMonthValue.Month
+                                                                       && e.StartDatetime.Value.Year == currentMonthValue.Year)
+                                                           .SelectMany(a => a.OrdersPartsEmployeesInfos.Select(x => x.ProductionТime))
+                                                           .SumAsync(a => a.Minutes);
+
+            var pastAndCurrentEmployeeWorkingMinutes = new PastAndCurrentEmployeeWorkingMinutesDto()
+            {
+                PastMonthEmployeeWorkingMinutes = pastMonthEmployeeWorkingMinutes,
+                CurrentMonthEmployeeWorkingMinutes = currentMonthEmployeeWorkingMinutes
+            };
+
+
+            return pastAndCurrentEmployeeWorkingMinutes;
+        }
+    }
+}
