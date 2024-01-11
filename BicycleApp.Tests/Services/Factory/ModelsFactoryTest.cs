c7fc09cfbde44a677eac6e571751ef3151a8aea4
Todorov135 <t.todorov135@gmail.com>
2024-01-11 09:29:57 +0200

    Add tests.

--- BicycleApp.Tests/Services/Factory/ModelsFactoryTest.cs
+++ BicycleApp.Tests/Services/Factory/ModelsFactoryTest.cs
@@ -1,7 +1,42 @@
 ﻿namespace BicycleApp.Tests.Services.Factory
 {
+    using BicycleApp.Services.Contracts;
+    using BicycleApp.Services.Models;
+    using BicycleApp.Services.Services.Factory;
+
     public class ModelsFactoryTest
     {
+        private readonly IModelsFactory _modelFactory;
+
+        public ModelsFactoryTest()
+        {
+            _modelFactory = new ModelsFactory();
+        }
+
+        [Test]
+        public void CreateNewBikeStandartModel_ShouldCreateBikeStandartModel()
+        {
+            //Arrange
+
+            string description = "Test description";
+            string imageUrl = "https://testbike/test/image.jpg";
+            string modelName = "Test model name";
+            decimal price = 152.26M;
+
+            BikeStandartTypeAddDto bike = new BikeStandartTypeAddDto
+            {
+                Description = description,
+                ImageUrl = imageUrl,
+                ModelName = modelName,
+                Price = price
+            };
+
+            //Act
+
+            var actual = _modelFactory.CreateNewBikeStandartModel(bike);
+
+            //Assert
+        }
 
     }
 }
